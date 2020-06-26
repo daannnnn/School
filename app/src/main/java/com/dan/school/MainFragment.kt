7a4d5e18252663af713936ea15197933afa944e0
Dan Artillaga <artillagadann@gmail.com>
2020-06-26 09:32:39 +0800

    implement bottom navigation

---
+++ app/src/main/java/com/dan/school/MainFragment.kt
@@ -0,0 +1,29 @@
+package com.dan.school
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        NavigationUI.setupWithNavController(
+            bottom_navigation,
+            childFragmentManager
+                .findFragmentById(R.id.bottom_navigation_fragment)!!.findNavController()
+        )
+    }
+}
