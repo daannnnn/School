e318f61bc1952ab25058a97e0e2a34f48c228ea7
Dan Artillaga <artillagadann@gmail.com>
2020-06-26 11:23:24 +0800

    implement floating action button

--- app/src/main/java/com/dan/school/MainFragment.kt
+++ app/src/main/java/com/dan/school/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -25,5 +26,17 @@ class MainFragment : Fragment() {
             childFragmentManager
                 .findFragmentById(R.id.bottom_navigation_fragment)!!.findNavController()
         )
+
+        fab_menu_homework.setOnClickListener {
+            Toast.makeText(context, "Homework Clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        fab_menu_exam.setOnClickListener {
+            Toast.makeText(context, "Exam Clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        fab_menu_task.setOnClickListener {
+            Toast.makeText(context, "Task Clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
