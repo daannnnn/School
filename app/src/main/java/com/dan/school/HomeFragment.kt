47ee937dba5cf499a369572e42e0786e21fd7c39
Dan Artillaga <artillagadann@gmail.com>
2020-06-27 13:48:53 +0800

    change color when selecting tab

--- app/src/main/java/com/dan/school/HomeFragment.kt
+++ app/src/main/java/com/dan/school/HomeFragment.kt
@@ -1,13 +1,18 @@
 package com.dan.school
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
+    val categoryColors = ArrayList<Int>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,12 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // colors for each category
+        categoryColors.add(ContextCompat.getColor(requireContext(), R.color.homeworkColor))
+        categoryColors.add(ContextCompat.getColor(requireContext(), R.color.examColor))
+        categoryColors.add(ContextCompat.getColor(requireContext(), R.color.taskColor))
+
+        // [START configure TabLayout and ViewPager]
         val homeworksFragment = HomeworksFragment()
         val examsFragment = ExamsFragment()
         val tasksFragment = TasksFragment()
@@ -28,7 +39,7 @@ class HomeFragment : Fragment() {
         mListFragments.add(examsFragment)
         mListFragments.add(tasksFragment)
 
-        val mListFragmentTitles = ArrayList<String>()
+        val mListFragmentTitles = ArrayList<String>()  // fragment titles
         mListFragmentTitles.add(getString(R.string.homeworks))
         mListFragmentTitles.add(getString(R.string.exams))
         mListFragmentTitles.add(getString(R.string.tasks))
@@ -37,5 +48,19 @@ class HomeFragment : Fragment() {
 
         viewPager.adapter = adapter
         tabLayout.setupWithViewPager(viewPager)
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                // change color when selecting tab
+                val position = tab!!.position
+                tabLayout.setSelectedTabIndicatorColor(categoryColors[position])
+                tabLayout.setTabTextColors(tabLayout.tabTextColors!!.defaultColor, categoryColors[position])
+            }
+        })
+        // [END configure TabLayout and ViewPager]
     }
 }
