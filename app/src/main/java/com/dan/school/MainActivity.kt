de9b0070d8916ed737dcb1b98736009319874db6
Dan Artillaga <artillagadann@gmail.com>
2020-06-28 14:44:27 +0800

    implement navigation drawer

--- app/src/main/java/com/dan/school/MainActivity.kt
+++ app/src/main/java/com/dan/school/MainActivity.kt
@@ -2,10 +2,39 @@ package com.dan.school
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(navigationView, navController)
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+        navigationView.menu.getItem(0).isChecked = true
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        item.isChecked = true
+        drawerLayout.closeDrawers()
+
+        when (item.itemId) {
+            R.id.overviewFragment -> navController.navigate(R.id.mainFragment)
+            R.id.settingsFragment -> Toast.makeText(this, "Settings Clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        return true
     }
 }
