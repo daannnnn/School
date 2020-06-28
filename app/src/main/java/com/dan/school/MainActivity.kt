1708758814a7e4c42af702cce19d81c3b809a996
Dan Artillaga <artillagadann@gmail.com>
2020-06-28 19:51:45 +0800

    remove navigation component on navigation drawer

--- app/src/main/java/com/dan/school/MainActivity.kt
+++ app/src/main/java/com/dan/school/MainActivity.kt
@@ -1,40 +1,61 @@
 package com.dan.school
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var navController: NavController
+    private val fragmentManager: FragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        NavigationUI.setupWithNavController(navigationView, navController)
-
         navigationView.setNavigationItemSelectedListener(this)
-
         navigationView.menu.getItem(0).isChecked = true
+        fragmentManager.beginTransaction().add(R.id.frameLayoutContent, MainFragment(), "main").commit()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         item.isChecked = true
-        drawerLayout.closeDrawers()
-
         when (item.itemId) {
-            R.id.overviewFragment -> navController.navigate(R.id.mainFragment)
-            R.id.settingsFragment -> Toast.makeText(this, "Settings Clicked!", Toast.LENGTH_SHORT).show()
+            R.id.mainFragment -> {
+                setFragment("main")
+            }
+            R.id.settingsFragment -> {
+                setFragment("settings")
+            }
         }
-
+        drawerLayout.closeDrawers()
         return true
+    }
+    private fun setFragment(tag: String) {
+        if (tag == "main") {
+            if (fragmentManager.findFragmentByTag("main") != null) {
+                fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("main")!!).commit()
+            } else {
+                fragmentManager.beginTransaction().add(R.id.frameLayoutContent, MainFragment(), "main").commit()
+            }
+            if(fragmentManager.findFragmentByTag("settings") != null){
+                fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("settings")!!).commit()
+            }
+        } else if (tag == "settings") {
+            if (fragmentManager.findFragmentByTag("settings") != null) {
+                fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("settings")!!).commit()
+            } else {
+                fragmentManager.beginTransaction().add(R.id.frameLayoutContent, SettingsFragment(), "settings").commit()
+            }
+            if(fragmentManager.findFragmentByTag("main") != null){
+                fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("main")!!).commit()
+            }
+        }
     }
 }
