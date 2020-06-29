7fa241b70bed6a616bf7b0d820b67acc097be10d
Dan Artillaga <artillagadann@gmail.com>
2020-06-30 06:36:40 +0800

    create AddHomeworkFragment, AddExamFragment, and AddTaskFragment

--- app/src/main/java/com/dan/school/AddHomeworkFragment.kt
+++ app/src/main/java/com/dan/school/AddHomeworkFragment.kt
@@ -6,13 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class SettingsFragment : Fragment() {
+class AddHomeworkFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.fragment_add_homework, container, false)
     }
 }
