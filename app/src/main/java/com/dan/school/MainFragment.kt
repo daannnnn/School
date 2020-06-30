40848c2ca7e509b4f7457c33af9acfcdd423e657
Dan Artillaga <artillagadann@gmail.com>
2020-06-30 09:31:44 +0800

    remove navigation component on bottom navigation

--- app/src/main/java/com/dan/school/MainFragment.kt
+++ app/src/main/java/com/dan/school/MainFragment.kt
@@ -2,19 +2,22 @@ package com.dan.school
 
 import android.animation.Animator
 import android.animation.ObjectAnimator
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.fragment_main.*
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     var isFABOpen = false
+    private lateinit var mChildFragmentManager: FragmentManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,13 +26,13 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mChildFragmentManager = childFragmentManager
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        NavigationUI.setupWithNavController(
-            bottom_navigation,
-            childFragmentManager
-                .findFragmentById(R.id.bottom_navigation_fragment)!!.findNavController()
-        )
 
         // Listeners
         fab_menu_homework.setOnClickListener {
@@ -50,6 +53,25 @@ class MainFragment : Fragment() {
         shadowView.setOnClickListener {
             clickFAB()
         }
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+
+        // Show HomeFragment
+        mChildFragmentManager.beginTransaction().add(R.id.frameLayoutBottomNavigation, HomeFragment(), "home").commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.homeFragment -> {
+                setFragment("home")
+            }
+            R.id.calendarFragment -> {
+                setFragment("calendar")
+            }
+            R.id.agendaFragment -> {
+                setFragment("agenda")
+            }
+        }
+        return true
     }
 
     private fun clickFAB() {
@@ -73,6 +95,46 @@ class MainFragment : Fragment() {
             })
             shadowView.isClickable = false
             shadowView.isFocusable = false
+        }
+    }
+
+    private fun setFragment(tag: String) {
+        if (tag == "home") {
+            if (mChildFragmentManager.findFragmentByTag("home") != null) {
+                mChildFragmentManager.beginTransaction().show(mChildFragmentManager.findFragmentByTag("home")!!).commit()
+            } else {
+                mChildFragmentManager.beginTransaction().add(R.id.frameLayoutBottomNavigation, HomeFragment(), "home").commit()
+            }
+            if(mChildFragmentManager.findFragmentByTag("calendar") != null){
+                mChildFragmentManager.beginTransaction().hide(mChildFragmentManager.findFragmentByTag("calendar")!!).commit()
+            }
+            if(mChildFragmentManager.findFragmentByTag("agenda") != null){
+                mChildFragmentManager.beginTransaction().hide(mChildFragmentManager.findFragmentByTag("agenda")!!).commit()
+            }
+        } else if (tag == "calendar") {
+            if (mChildFragmentManager.findFragmentByTag("calendar") != null) {
+                mChildFragmentManager.beginTransaction().show(mChildFragmentManager.findFragmentByTag("calendar")!!).commit()
+            } else {
+                mChildFragmentManager.beginTransaction().add(R.id.frameLayoutBottomNavigation, CalendarFragment(), "calendar").commit()
+            }
+            if(mChildFragmentManager.findFragmentByTag("home") != null){
+                mChildFragmentManager.beginTransaction().hide(mChildFragmentManager.findFragmentByTag("home")!!).commit()
+            }
+            if(mChildFragmentManager.findFragmentByTag("agenda") != null){
+                mChildFragmentManager.beginTransaction().hide(mChildFragmentManager.findFragmentByTag("agenda")!!).commit()
+            }
+        } else if (tag == "agenda") {
+            if (mChildFragmentManager.findFragmentByTag("agenda") != null) {
+                mChildFragmentManager.beginTransaction().show(mChildFragmentManager.findFragmentByTag("agenda")!!).commit()
+            } else {
+                mChildFragmentManager.beginTransaction().add(R.id.frameLayoutBottomNavigation, AgendaFragment(), "agenda").commit()
+            }
+            if(mChildFragmentManager.findFragmentByTag("home") != null){
+                mChildFragmentManager.beginTransaction().hide(mChildFragmentManager.findFragmentByTag("home")!!).commit()
+            }
+            if(mChildFragmentManager.findFragmentByTag("calendar") != null){
+                mChildFragmentManager.beginTransaction().hide(mChildFragmentManager.findFragmentByTag("calendar")!!).commit()
+            }
         }
     }
 }
