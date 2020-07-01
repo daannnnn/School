27f4b111111a2e9d936502b6ec03aac05e6ba5f3
Dan Artillaga <artillagadann@gmail.com>
2020-07-01 13:36:54 +0800

    handle back press

--- app/src/main/java/com/dan/school/MainNavigationFragment.kt
+++ app/src/main/java/com/dan/school/MainNavigationFragment.kt
@@ -1,10 +1,12 @@
 package com.dan.school
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 class MainNavigationFragment : Fragment() {
 
@@ -13,5 +15,21 @@ class MainNavigationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main_navigation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true /* enabled by default */) {
+                override fun handleOnBackPressed() {
+                    if (childFragmentManager.findFragmentById(R.id.nav_host_fragment)?.childFragmentManager?.backStackEntryCount != 0) {
+                        Navigation.findNavController(requireActivity(), R.id.nav_host_fragment).popBackStack()
+                    } else {
+                        requireActivity().finish()
+                    }
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
     }
 }
