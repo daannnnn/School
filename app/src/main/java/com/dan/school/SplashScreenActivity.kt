a8bb0264e1cf915deb5774e34cd9bb8bdce7d984
Dan Artillaga <artillagadann@gmail.com>
2020-08-09 17:54:59 +0800

    add splash screen

---
+++ app/src/main/java/com/dan/school/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.dan.school
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        finish()
+    }
+}
