5d60fafff2329302058e9d1f35aa0ea729f7f463
Dan Artillaga <artillagadann@gmail.com>
2020-08-12 12:57:57 +0800

    change spinner to TextSwitcher

--- app/src/main/java/com/dan/school/fragments/CompletedFragment.kt
+++ app/src/main/java/com/dan/school/fragments/CompletedFragment.kt
@@ -1,18 +1,32 @@
 package com.dan.school.fragments
 
 import android.animation.ObjectAnimator
+import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.ViewSwitcher
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.DialogFragment
 import com.dan.school.R
 import kotlinx.android.synthetic.main.fragment_completed.*
 
+
 class CompletedFragment : DialogFragment() {
 
     private var isOptionsExpanded = false
+    private lateinit var sortByArray: Array<String>
+    private var selectedIndex = 0
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sortByArray = resources.getStringArray(R.array.sort_by_array)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +38,26 @@ class CompletedFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        textSwitcherSortBy.setFactory {
+            val textView = TextView(requireContext())
+            textView.apply {
+                setTextColor(ContextCompat.getColor(requireContext(), R.color.colorPrimary))
+                setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
+                gravity = Gravity.CENTER_VERTICAL or Gravity.END
+                typeface = ResourcesCompat.getFont(requireContext(), R.font.cabin_bold)
+            }
+            textView
+        }
+        textSwitcherSortBy.setText(sortByArray[selectedIndex])
+
+        textSwitcherSortBy.setOnClickListener {
+            if (selectedIndex > 0) {
+                selectedIndex = 0
+            } else {
+                selectedIndex++
+            }
+            textSwitcherSortBy.setText(sortByArray[selectedIndex])
+        }
         buttonOptions.setOnClickListener {
             isOptionsExpanded = if (isOptionsExpanded) {
                 ObjectAnimator.ofFloat(imageViewOptions, "rotation", 0f)
@@ -39,15 +73,5 @@ class CompletedFragment : DialogFragment() {
                 true
             }
         }
-
-        ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.sort_by_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinnerSortBy.adapter = adapter
-        }
     }
-
 }
